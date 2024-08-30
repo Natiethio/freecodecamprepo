c22c50bbe7a6ec152116d3b9e8aa2dcd0f7f8cdf
Natiethio <natman093@gmail.com>
2024-08-30 17:45:35 +0300

    Create File Metadata Microservice.js

---
+++ File Metadata Microservice.js
@@ -0,0 +1,37 @@
+var express = require("express");
+const cors = require('cors')
+require("dotenv").config();
+const multer = require("multer");
+
+var app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
